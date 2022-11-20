23565442c5125fa7c8a1e42993ed004766c5b950
JooHyun Kim <kjh36102@gmail.com>
2022-11-20 20:15:40 +0900

    add module MP3_Controller

---
+++ modules/MP3_Controller/MP3_Controller_example.py
@@ -0,0 +1,7 @@
+from MP3_Controller import MP3_Controller
+mp3 = MP3_Controller('/home/iot/Desktop/iot_mask/TTS_records')#MP3파일 넣을 디렉토리 경로 입력
+
+mp3.play('barricade_close') # mp3 이름 참고해서 사용할 mp3 이름 입력
+mp3.play('barricade_open')
+mp3.play('high_temp_banned')
+mp3.play('high_temp_try_again')
